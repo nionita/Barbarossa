74f5eda49939cb11af22680e98900bea17fb8a51
Nicu Ionita <nicu@ionita.at>
2013-08-10 02:05:17 +0200

    SEE with parameter record, no warnings

--- Main/Barbarossa.hs
+++ Main/Barbarossa.hs
@@ -33,7 +33,7 @@ progName, progVersion, progVerSuff, progAuthor :: String
 progName    = "Barbarossa"
 progAuthor  = "Nicu Ionita"
 progVersion = "0.01"
-progVerSuff = "roots"
+progVerSuff = "xray"
 
 data Options = Options {
         optConfFile :: Maybe String,	-- config file
