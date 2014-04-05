3f54da00e9a1ee795624ff994459b4e37a709b93
Nicu Ionita <nicu@ionita.at>
2014-04-05 23:23:05 +0200

    Eval from plc8k on plcz

--- Main/Barbarossa.hs
+++ Main/Barbarossa.hs
@@ -38,7 +38,7 @@ progName, progVersion, progVerSuff, progAuthor :: String
 progName    = "Barbarossa"
 progAuthor  = "Nicu Ionita"
 progVersion = "0.2.0"
-progVerSuff = "plcz"
+progVerSuff = "plcz8"
 
 data Options = Options {
         optConfFile :: Maybe String,	-- config file
