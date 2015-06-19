63c4546a52681e91a92e43676550853edd5c9839
Nicu Ionita <nicu@ionita.at>
2015-06-19 22:35:23 +0200

    Evolve programm added, knight mobility try

--- Main/Barbarossa.hs
+++ Main/Barbarossa.hs
@@ -38,7 +38,7 @@ progName, progVersion, progVerSuff, progAuthor :: String
 progName    = "Barbarossa"
 progAuthor  = "Nicu Ionita"
 progVersion = "0.3.0"
-progVerSuff = "lkmr3"
+progVerSuff = "evo"
 
 data Options = Options {
         optConfFile :: Maybe String,	-- config file
