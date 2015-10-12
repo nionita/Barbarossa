807efac68ef7fb0b5aa06faf2f99d0d92ee4a5bc
Nicu Ionita <nicu@ionita.at>
2015-10-12 19:31:06 +0200

    Search: path selection only with combinePath

--- Main/Barbarossa.hs
+++ Main/Barbarossa.hs
@@ -38,7 +38,7 @@ progName, progVersion, progVerSuff, progAuthor :: String
 progName    = "Barbarossa"
 progAuthor  = "Nicu Ionita"
 progVersion = "0.4.0"
-progVerSuff = "nsflp"
+progVerSuff = "nsfpl"
 
 data Options = Options {
         optConfFile :: Maybe String,	-- config file
