a280fb80455c8a18936b255a018e8eaae4f7f47f
Nicu Ionita <nicu@ionita.at>
2015-12-09 21:35:00 +0100

    Futility pruning: another initial value

--- Search/Albeta.hs
+++ Search/Albeta.hs
@@ -81,7 +81,7 @@ futilMargins d s = s `unsafeShiftL` (d-1)
 
 -- Score statistics parameters for variable futility
 futIniVal, futMinVal, futDecayB, futDecayW :: Int
-futIniVal = 100
+futIniVal = 50
 futMinVal = 30
 futDecayB = 10
 futDecayW = (1 `unsafeShiftL` futDecayB) - 1
