600c2a6cc0894e007629c7124e6c8ff79cfd9017
Nicu Ionita <nicu@ionita.at>
2017-01-04 13:02:28 +0100

    Eval weights: same, third SGD run

--- Main/Barbarossa.hs
+++ Main/Barbarossa.hs
@@ -38,7 +38,7 @@ progName, progVersion, progVerSuff, progAuthor :: String
 progName    = "Barbarossa"
 progAuthor  = "Nicu Ionita"
 progVersion = "0.5.0"
-progVerSuff = "v11b"
+progVerSuff = "v11c"
 
 data Options = Options {
         optConfFile :: Maybe String,	-- config file
