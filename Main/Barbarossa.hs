6b807585518d0c069a1eb2f5d24b2c5e050707f8
Nicu Ionita <nicu@ionita.at>
2017-01-04 13:10:18 +0100

    Eval weights with SGD: 5M pos, shift 0.2

--- Main/Barbarossa.hs
+++ Main/Barbarossa.hs
@@ -38,7 +38,7 @@ progName, progVersion, progVerSuff, progAuthor :: String
 progName    = "Barbarossa"
 progAuthor  = "Nicu Ionita"
 progVersion = "0.5.0"
-progVerSuff = "v11c"
+progVerSuff = "v12a"
 
 data Options = Options {
         optConfFile :: Maybe String,	-- config file
