af645a2119be368b5dc53bd9f5545f3bd7182710
Nicu Ionita <nicu@ionita.at>
2017-08-21 20:15:34 +0200

    Limit LMR initial level for higher drafts

--- Main/Barbarossa.hs
+++ Main/Barbarossa.hs
@@ -39,7 +39,7 @@ progName, progVersion, progVerSuff, progAuthor :: String
 progName    = "Barbarossa"
 progAuthor  = "Nicu Ionita"
 progVersion = "0.5.0"
-progVerSuff = "plmr"
+progVerSuff = "hlmr"
 
 data Options = Options {
         optConfFile :: Maybe String,	-- config file
