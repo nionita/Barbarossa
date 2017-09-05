3d570e2fae337b527663e1a0c899c6a975bf155d
Nicu Ionita <nicu@ionita.at>
2017-09-06 01:12:41 +0200

    Eval: half values for most params

--- Eval/Eval.hs
+++ Eval/Eval.hs
@@ -559,7 +559,7 @@ spaceBlack !mpawns !matts !ypatts !yatts = sv
 spaceVals :: UArray Int Int
 spaceVals = listArray (0, 24) $ map f [1..25]
     where f x = round $ spf * (sqrt x - 1)
-          spf = 300 :: Double
+          spf = 200 :: Double
 
 -------- Attacks to adverse squares ----------
 data Advers = Advers
