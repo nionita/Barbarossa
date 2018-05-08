528125f438871204fedbe5a0f0cca6c504f2b3e4
Nicu Ionita <nicu@ionita.at>
2018-05-09 00:57:51 +0200

    Same eval params with DSPSA
    2k steps, normal, 0.05 final step

--- Main/Barbarossa.hs
+++ Main/Barbarossa.hs
@@ -39,7 +39,7 @@ progName, progVersion, progVerSuff, progAuthor :: String
 progName    = "Barbarossa"
 progAuthor  = "Nicu Ionita"
 progVersion = "0.5.0"
-progVerSuff = "tbk2"
+progVerSuff = "tbk3"
 
 data Options = Options {
         optConfFile :: Maybe String,	-- config file
