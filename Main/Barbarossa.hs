9547698090fab4f5798cb88f73970467652e653d
Nicu Ionita <nicu@ionita.at>
2019-02-08 21:14:46 +0100

    One try to optimize gen & sort
    Small optimizations with strictness and node type conditions

--- Main/Barbarossa.hs
+++ Main/Barbarossa.hs
@@ -39,7 +39,7 @@ progName, progVersion, progVerSuff, progAuthor :: String
 progName    = "Barbarossa"
 progAuthor  = "Nicu Ionita"
 progVersion = "0.5.0"
-progVerSuff = "idp4"
+progVerSuff = "ogs"
 
 data Options = Options {
         optConfFile :: Maybe String,	-- config file
