f3409a85c462f476ca535ca8a72650f41734a37f
Nicu Ionita <nicu@ionita.at>
2020-01-02 10:21:31 +0100

    Bayes optimization (GP): the optimum
    Bishop related weights: bishop mobility, bishop center attacks, bishop
    pair and bishop pawns

--- Main/Barbarossa.hs
+++ Main/Barbarossa.hs
@@ -39,7 +39,7 @@ progName, progVersion, progVerSuff, progAuthor :: String
 progName    = "Barbarossa"
 progAuthor  = "Nicu Ionita"
 progVersion = "0.6.0"
-progVerSuff = "bpbto"
+progVerSuff = "bpbtoo"
 
 data Options = Options {
         optConfFile :: Maybe String,	-- config file
