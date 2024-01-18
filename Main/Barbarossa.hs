b570ed907a3fd9eb8b42d04bf5be9ce317876f8e
Nicu Ionita <nicu@ionita.at>
2024-01-18 21:59:40 +0100

    Optimize king opennes & king mobility
    60 steps, 200 games per step, fixed noise
    Best performing version

--- Main/Barbarossa.hs
+++ Main/Barbarossa.hs
@@ -39,7 +39,7 @@ progName, progVersion, progVerSuff, progAuthor :: String
 progName    = "Barbarossa"
 progAuthor  = "Nicu Ionita"
 progVersion = "0.7.0"
-progVerSuff = "kimom"
+progVerSuff = "kimom1b"
 
 data Options = Options {
         optConfFile :: Maybe String,	-- config file
